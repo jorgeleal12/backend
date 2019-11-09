e7337c5a3a79887c1ac0f3cb21802e5b697646cc
jorgeleal12 <sistemas@grupoempresarialcyc.com>
2019-11-09 11:43:20 -0500

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('idusers');
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('state');
+            $table->integer('rol_idrol');
+            $table->integer('company_idcompany');
+            $table->string('id')->unique();
+            $table->integer('type');
+            $table->string('token_fir');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    // $table->increments('idusers');
+    // $table->string('name');
+    // $table->string('last_name');
+    // $table->string('email');
+    // $table->string('password');
+    // $table->string('state');
+    // $table->integer('rol_idrol');
+    // $table->integer('company_idcompany');
+    // $table->string('id')->unique();
+    // $table->integer('type');
+    // $table->string('token_fir');
+    // $table->rememberToken();
+    // $table->timestamps();
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        User::truncate();
+        User::create([
+            'email'     => 'admin@admin.com',
+            'password'  => Hash::make('adminadmin'),
+            'name'      => 'andres',
+            'last_name' => 'leal',
+            'state'     => '1',
+            'id'        => '1039679695',
+        ]);
+    }
+}
