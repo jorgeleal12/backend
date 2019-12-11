b2dc4b8cff49d635f1ebaf1a90a878c01eb1aed4
jorgeleal12 <sistemas@grupoempresarialcyc.com>
2019-12-11 17:05:22 -0500

    update

---
+++ app/Http/Controllers/NewControllers/Inspection/InspectionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\NewControllers\Inspection;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class InspectionController extends Controller
+{
+    //
+}
