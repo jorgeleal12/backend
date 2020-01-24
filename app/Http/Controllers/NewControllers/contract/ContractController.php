37be2b019f4025491da060f0b136ea9027d63937
root <sistemas@grupoempresarialcyc.com>
2020-01-23 22:35:10 -0500

    update

--- app/Http/Controllers/NewControllers/contract/ContractController.php
+++ app/Http/Controllers/NewControllers/contract/ContractController.php
@@ -6,62 +6,66 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class ContractController extends Controller
-{
+class ContractController extends Controller {
 
-    public function create(Request $request)
-    {
-        $company_idcompany = $request->input("company_idcompany");
-        $contract_name     = $request->input("contract_name");
+    public function create( Request $request ) {
+        $company_idcompany = $request->input( 'company_idcompany' );
+        $contract_name     = $request->input( 'contract_name' );
 
-        $insert = DB::table('contract')
-            ->insert([
-                'contract_name'     => $contract_name,
-                'company_idcompany' => $company_idcompany,
-            ]);
+        $insert = DB::table( 'contract' )
+        ->insert( [
+            'contract_name'     => $contract_name,
+            'company_idcompany' => $company_idcompany,
+        ] );
 
-        return response()->json(['status' => 'ok', 'response' => true], 200);
+        return response()->json( ['status' => 'ok', 'response' => true], 200 );
     }
 
-    public function searchs(Request $request)
-    {
-        $search = DB::table('contract')
-            ->join('company', 'company.idcompany', '=', 'contract.company_idcompany')
-            ->get();
+    public function searchs( Request $request ) {
+        $search = DB::table( 'contract' )
+        ->join( 'company', 'company.idcompany', '=', 'contract.company_idcompany' )
+        ->get();
 
-        return response()->json(['status' => 'ok', 'response' => $search], 200);
+        return response()->json( ['status' => 'ok', 'response' => $search], 200 );
     }
 
-    public function onchangesearchs(Request $request)
-    {
-        $search = DB::table('contract')
-            ->join('company', 'company.idcompany', '=', 'contract.company_idcompany')
-            ->get();
+    public function list_contract( Request $request ) {
+        $company = $request->input( 'company' );
 
-        return response()->json(['status' => 'ok', 'response' => $search], 200);
+        $search = DB::table( 'contract' )
+        ->where( 'company_idcompany', $company )
+        ->get();
+
+        return response()->json( ['status' => 'ok', 'response' => $search], 200 );
     }
 
-    public function update(Request $request)
-    {
-        $contract_name = $request->input("contract_name");
-        $idcontract    = $request->input("idcontract");
+    public function onchangesearchs( Request $request ) {
+        $search = DB::table( 'contract' )
+        ->join( 'company', 'company.idcompany', '=', 'contract.company_idcompany' )
+        ->get();
 
-        $update = DB::table('contract')
-            ->where('idcontract', $idcontract)
-            ->update([
-                'contract_name' => $contract_name,
-            ]);
-        return response()->json(['status' => 'ok', 'response' => true], 200);
+        return response()->json( ['status' => 'ok', 'response' => $search], 200 );
     }
 
-    public function search_contracts(Request $request)
-    {
+    public function update( Request $request ) {
+        $contract_name = $request->input( 'contract_name' );
+        $idcontract    = $request->input( 'idcontract' );
 
-        $company = $request->input("company");
+        $update = DB::table( 'contract' )
+        ->where( 'idcontract', $idcontract )
+        ->update( [
+            'contract_name' => $contract_name,
+        ] );
+        return response()->json( ['status' => 'ok', 'response' => true], 200 );
+    }
 
-        $search = DB::table('contract')
-            ->where('company_idcompany', $company)
-            ->get();
-        return response()->json(['status' => 'ok', 'response' => $search], 200);
+    public function search_contracts( Request $request ) {
+
+        $company = $request->input( 'company' );
+
+        $search = DB::table( 'contract' )
+        ->where( 'company_idcompany', $company )
+        ->get();
+        return response()->json( ['status' => 'ok', 'response' => $search], 200 );
     }
 }
