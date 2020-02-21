417e9c164318bdd3871a2e69bf018d57cf7cb4bb
jorgeleal12 <sistemas@grupoempresarialcyc.com>
2020-02-20 21:47:04 -0500

    reset paswword

--- app/Http/Controllers/NewControllers/user/users/UsersController.php
+++ app/Http/Controllers/NewControllers/user/users/UsersController.php
@@ -7,106 +7,72 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UsersController extends Controller {
+class UsersController extends Controller
+{
 
-    public function __construct() {
-        $this->middleware( 'jwt', ['except' => ['login']] );
+    public function __construct()
+    {
+        $this->middleware('jwt', ['except' => ['login']]);
     }
 
-    public function create( Request $request ) {
+    public function create(Request $request)
+    {
 
-        $name      = $request->input( 'name' );
-        $last_name = $request->input( 'last_name' );
-        $email     = $request->input( 'email' );
+        $name      = $request->input('name');
+        $last_name = $request->input('last_name');
+        $email     = $request->input('email');
         //$password          = Hash::make( $request->password );
-        $password          = $request->input( 'password' );
-        $state             = $request->input( 'state' );
-        $rol_idrol         = $request->input( 'rol_idrol' );
-        $company_idcompany = $request->input( 'company_idcompany' );
-        $id                = $request->input( 'id' );
+        $password          = $request->input('password');
+        $state             = $request->input('state');
+        $rol_idrol         = $request->input('rol_idrol');
+        $company_idcompany = $request->input('company_idcompany');
+        $id                = $request->input('id');
         //cedula o identificacion del usuario
-        $contract          = $request->input( 'selectedUser' );
-        $type              = $request->input( 'type' );
+        $contract = $request->input('selectedUser');
+        $type     = $request->input('type');
 
-        $insert = DB::table( 'users' )
-        ->insertGetid( [
-            'name'              => $name,
-            'last_name'         => $last_name,
-            'email'             => $email,
-            'password'          => Hash::make( $password ),
-            'state'             => $state,
-            'rol_idrol'         => $rol_idrol,
-            'company_idcompany' => $company_idcompany,
-            'id'                => $id,
-            'type'              => $type,
-        ] );
+        $insert = DB::table('users')
+            ->insertGetid([
+                'name'              => $name,
+                'last_name'         => $last_name,
+                'email'             => $email,
+                'password'          => Hash::make($password),
+                'state'             => $state,
+                'rol_idrol'         => $rol_idrol,
+                'company_idcompany' => $company_idcompany,
+                'id'                => $id,
+                // 'type'              => $type,
+            ]);
 
-        $this->contract_user( $contract, $insert );
-        return response()->json( ['status' => 'ok', 'reponse' => true, 'result' => $insert], 200 );
+        $this->contract_user($contract, $insert);
+        return response()->json(['status' => 'ok', 'reponse' => true, 'result' => $insert], 200);
     }
 
-    public function contract_user( $contract, $insertid ) {
-        foreach ( $contract as $contracts ) {
+    public function contract_user($contract, $insertid)
+    {
+        foreach ($contract as $contracts) {
 
-            $search_contract = DB::table( 'contract' )
-            ->where( 'contract_name', $contracts )
-            ->first();
-
-            $insert = DB::table( 'contract_user' )
-            ->insert( [
-                'users_idusers' => $insertid,
-                'idcontract'    => $search_contract->idcontract,
-            ] );
+            $insert = DB::table('contract_user')
+                ->insert([
+                    'users_idusers' => $insertid,
+                    'idcontract'    => $contracts,
+                ]);
         }
     }
 
-    public function delete_contract( Request $request ) {
+    public function searchs(Request $request)
+    {
 
-        $idusers    = $request->input( 'idusers' );
-        $idcontract = $request->input( 'idcontract' );
+        $search = DB::table('users')
+            ->leftjoin('rol', 'rol.idrol', '=', 'users.rol_idrol')
+            ->select('users.*', 'rol.*', 'users.state as id_state', DB::raw('(CASE WHEN users.state = "1" THEN "Activo" ELSE "Cancelado" END) AS state'))
+            ->get();
 
-        $search = DB::table( 'contract_user' )
-        ->where( 'users_idusers', $idusers )
-        ->where( 'idcontract', $idcontract )
-        ->first();
-        // var_dump( $search );
-
-        if ( $search ) {
-            $delete = DB::table( 'contract_user' )
-            ->where( 'idcontract', $idcontract )
-            ->where( 'users_idusers', $idusers )
-            ->delete();
-            // $delete = DB::table( 'contract_user' )
-            //     ->where( 'idcontract', $contracts )
-            //     ->where( 'users_idusers', $idusers )
-            //     ->delete();
-            // } else {
-            //     $insert = DB::table( 'contract_user' )
-            //         ->insert( [
-            //             'users_idusers' => $idusers,
-            //             'idcontract'    => $contracts,
-            //         ] );
-        } else {
-            $insert = DB::table( 'contract_user' )
-            ->insert( [
-                'users_idusers' => $idusers,
-                'idcontract'    => $idcontract,
-            ] );
-        }
-
+        return response()->json(['status' => 'ok', 'reponse' => true, 'result' => $search], 200);
     }
 
-    public function searchs( Request $request ) {
-
-        $search = DB::table( 'users' )
-        ->leftjoin( 'rol', 'rol.idrol', '=', 'users.rol_idrol' )
-        ->select( 'users.*', 'rol.*', 'users.state as id_state', DB::raw( '(CASE WHEN users.state = "1" THEN "Activo" ELSE "Cancelado" END) AS state' ) )
-        ->get();
-
-        return response()->json( ['status' => 'ok', 'reponse' => true, 'result' => $search], 200 );
-    }
-
-    public function update( Request $request ) {
+    public function update(Request $request)
+    {
         $idusers           = $request->idusers;
         $name              = $request->name;
         $last_name         = $request->last_name;
@@ -116,84 +82,88 @@ class UsersController extends Controller {
         $company_idcompany = $request->company_idcompany;
         $id                = $request->id;
         //cedula o identificacion del usuario
-        $type              = $request->type;
-        $contract          = $request->selectedUser;
+        $type     = $request->type;
+        $contract = $request->selectedUser;
 
-        $insert = DB::table( 'users' )
-        ->where( 'idusers', $idusers )
-        ->update( [
-            'name'              => $name,
-            'last_name'         => $last_name,
-            'email'             => $email,
-            'state'             => $state,
-            'rol_idrol'         => $rol_idrol,
-            'company_idcompany' => $company_idcompany,
-            'id'                => $id,
-            'type'              => $type,
-        ] );
+        $insert = DB::table('users')
+            ->where('idusers', $idusers)
+            ->update([
+                'name'              => $name,
+                'last_name'         => $last_name,
+                'email'             => $email,
+                'state'             => $state,
+                'rol_idrol'         => $rol_idrol,
+                'company_idcompany' => $company_idcompany,
+                'id'                => $id,
+                // 'type'              => $type,
+            ]);
 
-        $this->update_contrat( $contract, $idusers );
+        $this->update_contrat($contract, $idusers);
 
-        return response()->json( ['status' => 'ok', 'reponse' => true], 200 );
+        return response()->json(['status' => 'ok', 'reponse' => true], 200);
     }
 
-    public function update_contrat( $contract, $idusers ) {
+    public function update_contrat($contract, $idusers)
+    {
 
-        $delete = DB::table( 'contract_user' )
-        ->where( 'users_idusers', $idusers )
-        ->delete();
+        $delete = DB::table('contract_user')
+            ->where('users_idusers', $idusers)
+            ->delete();
 
-        foreach ( $contract as $contracts ) {
+        foreach ($contract as $contracts) {
 
-            $insert = DB::table( 'contract_user' )
-            ->insert( [
-                'users_idusers' => $idusers,
-                'idcontract'    => $contracts,
-            ] );
+            $insert = DB::table('contract_user')
+                ->insert([
+                    'users_idusers' => $idusers,
+                    'idcontract'    => $contracts,
+                ]);
         }
 
     }
 
-    public function search_contract( Request $request ) {
+    public function search_contract(Request $request)
+    {
 
-        $idusers = $request->input( 'idusers' );
+        $idusers = $request->input('idusers');
 
-        $search = DB::table( 'contract_user' )
-        ->leftjoin( 'contract', 'contract.idcontract', '=', 'contract_user.idcontract' )
-        ->where( 'users_idusers', $idusers )
-        ->select( 'contract_user.idcontract', 'contract.contract_name' )
-        ->get();
+        $search = DB::table('contract_user')
+            ->leftjoin('contract', 'contract.idcontract', '=', 'contract_user.idcontract')
+            ->where('users_idusers', $idusers)
+            ->select('contract_user.idcontract', 'contract.contract_name')
+            ->get();
 
-        return response()->json( ['status' => 'ok', 'response' => $search], 200 );
+        return response()->json(['status' => 'ok', 'response' => $search], 200);
     }
 
-    public function delete( Request $request ) {
-        $iduser = $request->input( 'iduser' );
+    public function delete(Request $request)
+    {
+        $iduser = $request->input('iduser');
 
-        $delete = DB::table( 'users' )
-        ->where( 'idusers', $iduser )
-        ->delete();
-        return response()->json( ['status' => 'ok', 'response' => true], 200 );
+        $delete = DB::table('users')
+            ->where('idusers', $iduser)
+            ->delete();
+        return response()->json(['status' => 'ok', 'response' => true], 200);
 
     }
 
-    public function search_user( Request $request ) {
-        $cedula = $request->input( 'cedula' );
+    public function search_user(Request $request)
+    {
+        $cedula = $request->input('cedula');
 
-        $search = DB::table( 'employees' )
-        ->where( 'identification', $cedula )
-        ->first();
-
-        if ( $search ) {
-            $search_user = DB::table( 'users' )
-            ->where( 'id', $search->identification )
+        $search = DB::table('employees')
+            ->where('identification', $cedula)
             ->first();
 
-            if ( $search_user ) {
-                return response()->json( ['status' => 'ok', 'response' => false], 200 );
+        if ($search) {
+            $search_user = DB::table('users')
+                ->where('id', $search->identification)
+                ->first();
+
+            if ($search_user) {
+                return response()->json(['status' => 'ok', 'response' => false], 200);
             } else {
 
-                return response()->json( ['status' => 'ok', 'response' => true, 'result' => $search], 200 );
+                return response()->json(['status' => 'ok', 'response' => true, 'result' => $search], 200);
             }
         } else {
             echo '2';
