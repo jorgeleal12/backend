d611ecdcc81454b06cea5ec9b583afa636ac1936
Gustavo Vega <gustavo.vega@gmail.com>
2020-04-04 08:58:52 -0500

    rutas

--- routes/api.php
+++ routes/api.php
@@ -282,6 +282,21 @@ Route::group([
     Route::get('/client/autocomplete', 'NewControllers\client\ClientController@autocomplete');
     Route::get('/client/autocomplete_address', 'NewControllers\client\ClientController@autocomplete_address');
 
-   
+    //Rutas Gustavo
+    ///---------------------------materiales-------------------------------------------
+    Route::post('material/create', 'NewControllers\material\MaterialController@create');
+    Route::post('material/delete', 'NewControllers\material\MaterialController@delete');
+    Route::post('material/update', 'NewControllers\material\MaterialController@update');
+    Route::get('material/list_unit', 'NewControllers\material\MaterialController@list_unit');
+    Route::get('material/list_typeMaterial', 'NewControllers\material\MaterialController@list_typeMaterial');
+    Route::get('material/search', 'NewControllers\material\MaterialController@search');
+    Route::get('material/search_code', 'NewControllers\material\MaterialController@search_code');
+
+    ///---------------------------Providers-------------------------------------------
+    Route::get('provider/listProviders', 'NewControllers\provider\ProviderController@getLis');
+    Route::get('provider/search_provider', 'NewControllers\provider\ProviderController@search_provider');
+    Route::post('provider/create', 'NewControllers\provider\ProviderController@create');
+    Route::post('provider/update', 'NewControllers\provider\ProviderController@update');
+    Route::post('provider/delete', 'NewControllers\provider\ProviderController@delete');
     
 });
