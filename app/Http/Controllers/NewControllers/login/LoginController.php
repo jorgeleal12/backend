1a56e7ec2ed54260a522b666c275d2cdbbc791f7
jorgeleal12 <sistemas@grupoempresarialcyc.com>
2020-07-27 08:18:57 -0500

    update

--- app/Http/Controllers/NewControllers/login/LoginController.php
+++ app/Http/Controllers/NewControllers/login/LoginController.php
@@ -6,46 +6,49 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class LoginController extends Controller {
+class LoginController extends Controller
+{
 
-    public function login( Request $request ) {
+    public function login(Request $request)
+    {
 
-        $user     = $request->input( 'user' );
-        $pass     = $request->input( 'pass' );
-        $company  = $request->input( 'company' );
-        $contract = $request->input( 'contract' );
+        $user     = $request->input('user');
+        $pass     = $request->input('pass');
+        $company  = $request->input('company');
+        $contract = $request->input('contract');
 
-        $search = DB::table( 'users' )
-        ->join( 'contract_user', 'contract_user.users_idusers', '=', 'users.idusers' )
-        ->where( 'idcontract', $contract )
-        ->where( 'id', $user )
-        ->where( 'password', $pass )
-        ->where( 'company_idcompany', $company )
-        ->where( 'state', 1 )
-        ->select( 'users.id as cedula', 'users.name', 'users.last_name', 'users.company_idcompany', DB::raw( '"session" as id' ), 'users.idusers as idusers', 'idcontract' )
-        ->first();
+        $search = DB::table('users')
+            ->join('contract_user', 'contract_user.users_idusers', '=', 'users.idusers')
+            ->where('idcontract', $contract)
+            ->where('id', $user)
+            ->where('password', $pass)
+            ->where('company_idcompany', $company)
+            ->where('state', 1)
+            ->select('users.id as cedula', 'users.name', 'users.last_name', 'users.company_idcompany', DB::raw('"session" as id'), 'users.idusers as idusers', 'idcontract')
+            ->first();
 
-        if ( $search ) {
-            return response()->json( ['status' => 'ok', 'reponse' => true, 'result' => $search], 200 );
+        if ($search) {
+            return response()->json(['status' => 'ok', 'reponse' => true, 'result' => $search], 200);
         } else {
-            return response()->json( ['status' => 'ok', 'reponse' => false], 200 );
+            return response()->json(['status' => 'ok', 'reponse' => false], 200);
         }
 
     }
 
-    public function load_rol( Request $request ) {
-        $idusers = $request->input( 'idusers' );
+    public function load_rol(Request $request)
+    {
+        $idusers = $request->input('idusers');
 
-        $search = DB::table( 'users' )
-        ->join( 'rol', 'rol.idrol', '=', 'users.rol_idrol' )
-        ->join( 'rol_permission', 'rol_permission.rol_idrol', '=', 'rol.idrol' )
-        ->join( 'permission', 'permission.idpermission', '=', 'rol_permission.idpermission' )
+        $search = DB::table('users')
+            ->join('rol', 'rol.idrol', '=', 'users.rol_idrol')
+            ->join('rol_permission', 'rol_permission.rol_idrol', '=', 'rol.idrol')
+            ->join('permission', 'permission.idpermission', '=', 'rol_permission.idpermission')
 
-        ->where( 'idusers', $idusers )
-        ->select( 'rol.idrol', 'rol_permission.idrol_permission', 'rol_permission.save', 'rol_permission.delete', 'rol_permission.edit', 'permission.idpermission as id' )
-        ->get();
+            ->where('idusers', $idusers)
+            ->select('rol.idrol', 'rol_permission.idrol_permission', 'rol_permission.save', 'rol_permission.delete', 'rol_permission.edit', 'rol_permission.view', 'permission.idpermission as id')
+            ->get();
 
-        return response()->json( ['status' => 'ok', 'reponse' => $search], 200 );
+        return response()->json(['status' => 'ok', 'reponse' => $search], 200);
     }
 
 }
