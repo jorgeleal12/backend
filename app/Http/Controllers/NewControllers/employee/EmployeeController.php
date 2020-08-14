86a2c7224129abdc4ce2e52a20cc5906a8aaeddd
jorgeleal12 <sistemas@grupoempresarialcyc.com>
2020-08-13 21:54:38 -0500

    update

--- app/Http/Controllers/NewControllers/employee/EmployeeController.php
+++ app/Http/Controllers/NewControllers/employee/EmployeeController.php
@@ -6,11 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-use Carbon\Carbon;
 class EmployeeController extends Controller
 {
     //
-
 
     public function create(Request $request)
     {
@@ -119,8 +117,7 @@ class EmployeeController extends Controller
         $type_rh         = $request->type_rh;
         $education_level = $request->education_level;
         $location_az     = $request->location_az;
-        $expedition_date =date('Y-m-d', strtotime($request->input('expedition_date'))) == '1969-12-31' ? null : date('Y-m-d', strtotime($request->input('expedition_date')));
-
+        $expedition_date = date('Y-m-d', strtotime($request->input('expedition_date'))) == '1969-12-31' ? null : date('Y-m-d', strtotime($request->input('expedition_date')));
 
         $state             = $request->state;
         $charges_idcharges = $request->charges_idcharges;
@@ -170,9 +167,9 @@ class EmployeeController extends Controller
             ->where('name', 'like', '%' . $employee . '%')
             ->orWhere('identification', 'like', '%' . $employee . '%')
             ->select('employees.*', DB::raw('CONCAT(employees.name," ",employees.last_name) AS full_name')
-            ,DB::raw('DATE_FORMAT(employees.birthdate, "%Y/%m/%d") as birthdate')
-                     ,DB::raw('DATE_FORMAT(employees.expedition_date, "%Y/%m/%d") as expedition_date')
-                     ,DB::raw('DATE_FORMAT(employees.date_admission, "%Y/%m/%d") as date_admission'))
+                , DB::raw('DATE_FORMAT(employees.birthdate, "%Y/%m/%d") as birthdate')
+                , DB::raw('DATE_FORMAT(employees.expedition_date, "%Y/%m/%d") as expedition_date')
+                , DB::raw('DATE_FORMAT(employees.date_admission, "%Y/%m/%d") as date_admission'))
             ->take(10)
             ->get();
 
@@ -182,14 +179,12 @@ class EmployeeController extends Controller
     public function search_employee()
     {
 
-
-
         $search = DB::table('employees')
             ->orderBy('name', 'ASC')
-            ->select('employees.*',DB::raw('DATE_FORMAT(employees.birthdate, "%Y/%m/%d") as birthdate')
+            ->select('employees.*', DB::raw('DATE_FORMAT(employees.birthdate, "%Y/%m/%d") as birthdate')
 
-                     ,DB::raw('DATE_FORMAT(employees.expedition_date, "%Y/%m/%d") as expedition_date')
-                     ,DB::raw('DATE_FORMAT(employees.date_admission, "%Y/%m/%d") as date_admission') )
+                , DB::raw('DATE_FORMAT(employees.expedition_date, "%Y/%m/%d") as expedition_date')
+                , DB::raw('DATE_FORMAT(employees.date_admission, "%Y/%m/%d") as date_admission'))
             ->get();
 
         return response()->json(['status' => 'ok', 'response' => $search], 200);
