fec6703d63f96f8d784173dc4407dfaf6978ec4e
Andres Leal <sistemas@grupoempresarialcyc.com>
2021-08-18 22:12:26 -0500

    cambio de formato

--- app/Http/Controllers/NewControllers/Prints/PrintMaintenanceController.php
+++ app/Http/Controllers/NewControllers/Prints/PrintMaintenanceController.php
@@ -8,15 +8,12 @@ use Illuminate\Support\Facades\DB;
 
 class PrintMaintenanceController extends Fpdf
 {
-    protected $pdf;
+    //class PrintMaintenanceController extends Controller
 
-    public function __construct( \App\espacios $pdf ) {
-        $this->pdf = $pdf;
-
-    }
+    //
 
     function print(Request $request) {
-        //$this->pdf->Image('../public/imagenes/documentos/com-1.jpg', '2', '2', '205', '280', 'JPG');
+        // $this->Image('../public/imagenes/documentos/com-1.jpg', '2', '2', '205', '280', 'JPG');
 
         $idleaves = $request->idleaves;
 
@@ -39,19 +36,19 @@ class PrintMaintenanceController extends Fpdf
             ->take(6)
             ->get();
 
-       $this->pdf->AliasNbPages();
-       $this->pdf->AddPage();
-       $this->pdf->SetFont('Times', '', 12);
-       $this->pdf->SetAutoPageBreak(true, 10);
-       $this->pdf->Image('../public/images/formato1.jpg', '2', '2', '205', '280', 'JPG');
+        $this->AliasNbPages();
+        $this->AddPage();
+        $this->SetFont('Times', '', 12);
+        $this->SetAutoPageBreak(true, 10);
+        $this->Image('../public/images/formato1.jpg', '2', '2', '205', '280', 'JPG');
 
         $row = 7;
         foreach ($search_image as $search_image) {
 
             $document= explode('.',$search_image->name_image);
             if($document[1]!='pdf'){
-               $this->pdf->Image('https://ares-path.s3.amazonaws.com/Items/' . $search_image->name_image, $row, 44, 48, 40);
-               $this->pdf->Cell(4, 6, '', 0, 0);
+                $this->Image('https://ares-path.s3.amazonaws.com/Items/' . $search_image->name_image, $row, 44, 48, 40);
+                $this->Cell(4, 6, '', 0, 0);
                 $row = $row + 48;
                 $row++;
             }
@@ -61,33 +58,31 @@ class PrintMaintenanceController extends Fpdf
         $service_provided = str_replace('”','"',$search->service_provided);
         $name_materials = str_replace('“','"',$name_materials);
         $service_provided = str_replace('“','"',$service_provided);
-       $this->pdf->Ln(23);
-       $this->pdf->SetFont('Arial', '', 7);
-       $this->pdf->Cell(38, 6, '', 0, 0);
-       $this->pdf->Cell(23, 3.2,  utf8_decode($name_materials), 0, 0); 
-       $this->pdf->Ln(5.5);
-       $this->pdf->SetFont('Arial', '', 7);
-       $this->pdf->Cell(4, 6, '', 0, 0);
-       $this->pdf->Cell(23, 3.2,  utf8_decode($search->code), 0, 0);
+        $this->Ln(23);
+        $this->SetFont('Arial', '', 7);
+        $this->Cell(38, 6, '', 0, 0);
+        $this->Cell(23, 3.2,  utf8_decode($name_materials), 0, 0); 
+        $this->Ln(5.5);
+        $this->SetFont('Arial', '', 7);
+        $this->Cell(4, 6, '', 0, 0);
+        $this->Cell(23, 3.2,  utf8_decode($search->code), 0, 0);
 
-       $this->pdf->Ln(49.5);
-       $this->pdf->SetFont('Arial', '', 7);
-       $this->pdf->Cell(22, 6, '', 0, 0);
-       $this->pdf->Cell(70, 3.2, $acquisition_date, 0, 0);
-       $this->pdf->Cell(23, 3.2,  utf8_decode($search->name_provider), 0, 0);
+        $this->Ln(49.5);
+        $this->SetFont('Arial', '', 7);
+        $this->Cell(22, 6, '', 0, 0);
+        $this->Cell(70, 3.2, $acquisition_date, 0, 0);
+        $this->Cell(23, 3.2,  utf8_decode($search->name_provider), 0, 0);
 
-       $this->pdf->Ln(8.7);
-       $this->pdf->SetFont('Arial', '', 7);
-       $this->pdf->Cell(20, 6, '', 0, 0);
-        //$this->pdf->Cell(23, 3.2,  utf8_decode($service_provided), 0, 0);
-        $y_item111 =$this->pdf->GetY();
-       $this->pdf->MultiCell(150, 9, utf8_decode($service_provided), '', 'L', 0, 20);
-        $y_item211 =$this->pdf->GetY();
-        
-       $this->pdf->SetFont('Arial', '', 7);
-       $this->pdf->Cell(3, 6, '', 0, 0);
-       $this->pdf->Cell(70, 3.2, $search->brand, 0, 0);
-       $this->pdf->Cell(48, 6, '', 0, 0);
+        $this->Ln(8.7);
+        $this->SetFont('Arial', '', 8);
+        $this->Cell(20, 6, '', 0, 0);
+        $this->Cell(23, 3.2,  utf8_decode($service_provided), 0, 0);
+
+        $this->Ln(9.2);
+        $this->SetFont('Arial', '', 8);
+        $this->Cell(3, 6, '', 0, 0);
+        $this->Cell(70, 3.2, $search->brand, 0, 0);
+        $this->Cell(48, 6, '', 0, 0);
 
         $data_sheet = 'NO';
         $handbook   = 'NO';
@@ -102,17 +97,17 @@ class PrintMaintenanceController extends Fpdf
             $handbook = 'SI';
         }
 
-       $this->pdf->Cell(22, 3.2, $data_sheet, 0, 0);
-       $this->pdf->Cell(23, 3.2, $handbook, 0, 0);
+        $this->Cell(22, 3.2, $data_sheet, 0, 0);
+        $this->Cell(23, 3.2, $handbook, 0, 0);
 
-       $this->pdf->Ln(4.2);
-       $this->pdf->SetFont('Arial', '', 7);
-       $this->pdf->Cell(5, 6, '', 0, 0);
-       $this->pdf->Cell(15, 3.2, $search->serie, 0, 0);
+        $this->Ln(4.5);
+        $this->SetFont('Arial', '', 7);
+        $this->Cell(5, 6, '', 0, 0);
+        $this->Cell(15, 3.2, $search->serie, 0, 0);
 
         $line  = '3.2';
         $line2 = '8.4';
-       $this->pdf->Ln($line2);
+        $this->Ln($line2);
         foreach ($search_maintenance as $t) {
 
             $date_maintenance   = date('Y-m-d', strtotime($t->date_maintenance));
@@ -125,26 +120,26 @@ class PrintMaintenanceController extends Fpdf
                 $state_maintenance2 = 'X';
             }
 
-           $this->pdf->Ln();
-           $this->pdf->Ln(0.4);
-           $this->pdf->SetFont('Arial', '', 8);
-           $this->pdf->Cell(2, 6, '', 0, 0);
-           $this->pdf->Cell(60, 3.2, $date_maintenance, 0, 0);
-           $this->pdf->Cell(23, 3.2, $state_maintenance2, 0, 0);
+            $this->Ln();
+            $this->Ln(0.4);
+            $this->SetFont('Arial', '', 8);
+            $this->Cell(2, 6, '', 0, 0);
+            $this->Cell(60, 3.2, $date_maintenance, 0, 0);
+            $this->Cell(23, 3.2, $state_maintenance2, 0, 0);
 
-           $this->pdf->Ln($line);
-           $this->pdf->SetFont('Arial', '', 7);
-           $this->pdf->Cell(62, 6, '', 0, 0);
-           $this->pdf->Cell(17, 3.2, $state_maintenance1, 0, 0);
-           $this->pdf->Cell(40, 3.2, $t->who, 0, 0);
-           $this->pdf->Cell(45, 6, '', 0, 0);
-           $this->pdf->Cell(23, 3.2, $t->support, 0, 0);
-           $this->pdf->Ln(7);
+            $this->Ln($line);
+            $this->SetFont('Arial', '', 8);
+            $this->Cell(62, 6, '', 0, 0);
+            $this->Cell(17, 3.2, $state_maintenance1, 0, 0);
+            $this->Cell(40, 3.2, $t->who, 0, 0);
+            $this->Cell(45, 6, '', 0, 0);
+            $this->Cell(23, 3.2, $t->support, 0, 0);
+            $this->Ln(7);
 
-           $this->pdf->Cell(1, 6, '', 0, 0);
-            $y =$this->pdf->GetY();
-           $this->pdf->MultiCell(150, 3, utf8_decode($t->observations_responsable), 0, 1);
-            $y2 =$this->pdf->GetY();
+            $this->Cell(1, 6, '', 0, 0);
+            $y = $this->GetY();
+            $this->MultiCell(150, 3, utf8_decode($t->observations_responsable), 0, 1);
+            $y2 = $this->GetY();
 
             $space = (isset($y2) ? $y2 - $y : 0) - 0;
 
@@ -156,9 +151,9 @@ class PrintMaintenanceController extends Fpdf
 
                 $line2 = $line2 + 3;
             }
-            //$this->pdf->SetXY(150, $y);
-            // $this->pdf->Cell(45, 6, $space.' y2 '.$y2.' y1 '. $y, 0, 0);
-           $this->pdf->Ln($space);
+            // $this->SetXY(150, $y);
+            //  $this->Cell(45, 6, $space.' y2 '.$y2.' y1 '. $y, 0, 0);
+            $this->Ln($space);
 
             $line2 = $line2 + 3;
 
@@ -171,7 +166,7 @@ class PrintMaintenanceController extends Fpdf
 
     public function download(Request $request)
     {
-        //$this->pdf->Image('../public/imagenes/documentos/com-1.jpg', '2', '2', '205', '280', 'JPG');
+        // $this->Image('../public/imagenes/documentos/com-1.jpg', '2', '2', '205', '280', 'JPG');
 
         $idleaves = $request->idleaves;
 
@@ -194,19 +189,19 @@ class PrintMaintenanceController extends Fpdf
             ->take(6)
             ->get();
 
-       $this->pdf->AliasNbPages();
-       $this->pdf->AddPage();
-       $this->pdf->SetFont('Times', '', 12);
-       $this->pdf->SetAutoPageBreak(true, 10);
-       $this->pdf->Image('../public/images/formato1.jpg', '2', '2', '205', '280', 'JPG');
+        $this->AliasNbPages();
+        $this->AddPage();
+        $this->SetFont('Times', '', 12);
+        $this->SetAutoPageBreak(true, 10);
+        $this->Image('../public/images/formato1.jpg', '2', '2', '205', '280', 'JPG');
 
         $row = 7;
         foreach ($search_image as $search_image) {
 
             $document= explode('.',$search_image->name_image);
             if($document[1]!='pdf'){
-               $this->pdf->Image('https://ares-path.s3.amazonaws.com/Items/' . $search_image->name_image, $row, 44, 48, 40);
-               $this->pdf->Cell(4, 6, '', 0, 0);
+                $this->Image('https://ares-path.s3.amazonaws.com/Items/' . $search_image->name_image, $row, 44, 48, 40);
+                $this->Cell(4, 6, '', 0, 0);
                 $row = $row + 48;
                 $row++;
             }
@@ -216,31 +211,31 @@ class PrintMaintenanceController extends Fpdf
 
         $name_materials = str_replace('“','"',$name_materials);
         $service_provided = str_replace('“','"',$service_provided);
-       $this->pdf->Ln(23);
-       $this->pdf->SetFont('Arial', '', 7);
-       $this->pdf->Cell(38, 6, '', 0, 0);
-       $this->pdf->Cell(23, 3.2, utf8_decode($name_materials), 0, 0);
-       $this->pdf->Ln(5.5);
-       $this->pdf->SetFont('Arial', '', 7);
-       $this->pdf->Cell(4, 6, '', 0, 0);
-       $this->pdf->Cell(23, 3.2, utf8_decode($search->code), 0, 0);
+        $this->Ln(23);
+        $this->SetFont('Arial', '', 7);
+        $this->Cell(38, 6, '', 0, 0);
+        $this->Cell(23, 3.2, utf8_decode($name_materials), 0, 0);
+        $this->Ln(5.5);
+        $this->SetFont('Arial', '', 7);
+        $this->Cell(4, 6, '', 0, 0);
+        $this->Cell(23, 3.2, utf8_decode($search->code), 0, 0);
 
-       $this->pdf->Ln(49.5);
-       $this->pdf->SetFont('Arial', '', 7);
-       $this->pdf->Cell(22, 6, '', 0, 0);
-       $this->pdf->Cell(70, 3.2, $acquisition_date, 0, 0);
-       $this->pdf->Cell(23, 3.2, utf8_decode($search->name_provider), 0, 0);
+        $this->Ln(49.5);
+        $this->SetFont('Arial', '', 7);
+        $this->Cell(22, 6, '', 0, 0);
+        $this->Cell(70, 3.2, $acquisition_date, 0, 0);
+        $this->Cell(23, 3.2, utf8_decode($search->name_provider), 0, 0);
 
-       $this->pdf->Ln(8.7);
-       $this->pdf->SetFont('Arial', '', 8);
-       $this->pdf->Cell(20, 6, '', 0, 0);
-       $this->pdf->Cell(23, 3.2, utf8_decode($service_provided), 0, 0);
+        $this->Ln(8.7);
+        $this->SetFont('Arial', '', 8);
+        $this->Cell(20, 6, '', 0, 0);
+        $this->Cell(23, 3.2, utf8_decode($service_provided), 0, 0);
 
-       $this->pdf->Ln(9.2);
-       $this->pdf->SetFont('Arial', '', 8);
-       $this->pdf->Cell(3, 6, '', 0, 0);
-       $this->pdf->Cell(70, 3.2, $search->brand, 0, 0);
-       $this->pdf->Cell(48, 6, '', 0, 0);
+        $this->Ln(9.2);
+        $this->SetFont('Arial', '', 8);
+        $this->Cell(3, 6, '', 0, 0);
+        $this->Cell(70, 3.2, $search->brand, 0, 0);
+        $this->Cell(48, 6, '', 0, 0);
 
         $data_sheet = 'NO';
         $handbook   = 'NO';
@@ -255,17 +250,17 @@ class PrintMaintenanceController extends Fpdf
             $handbook = 'SI';
         }
 
-       $this->pdf->Cell(22, 3.2, $data_sheet, 0, 0);
-       $this->pdf->Cell(23, 3.2, $handbook, 0, 0);
+        $this->Cell(22, 3.2, $data_sheet, 0, 0);
+        $this->Cell(23, 3.2, $handbook, 0, 0);
 
-       $this->pdf->Ln(4.5);
-       $this->pdf->SetFont('Arial', '', 7);
-       $this->pdf->Cell(5, 6, '', 0, 0);
-       $this->pdf->Cell(15, 3.2, $search->serie, 0, 0);
+        $this->Ln(4.5);
+        $this->SetFont('Arial', '', 7);
+        $this->Cell(5, 6, '', 0, 0);
+        $this->Cell(15, 3.2, $search->serie, 0, 0);
 
         $line  = '3.2';
         $line2 = '8.4';
-       $this->pdf->Ln($line2);
+        $this->Ln($line2);
         foreach ($search_maintenance as $t) {
 
             $date_maintenance   = date('Y-m-d', strtotime($t->date_maintenance));
@@ -278,26 +273,26 @@ class PrintMaintenanceController extends Fpdf
                 $state_maintenance2 = 'X';
             }
 
-           $this->pdf->Ln();
-           $this->pdf->Ln(0.4);
-           $this->pdf->SetFont('Arial', '', 8);
-           $this->pdf->Cell(2, 6, '', 0, 0);
-           $this->pdf->Cell(60, 3.2, $date_maintenance, 0, 0);
-           $this->pdf->Cell(23, 3.2, $state_maintenance2, 0, 0);
+            $this->Ln();
+            $this->Ln(0.4);
+            $this->SetFont('Arial', '', 8);
+            $this->Cell(2, 6, '', 0, 0);
+            $this->Cell(60, 3.2, $date_maintenance, 0, 0);
+            $this->Cell(23, 3.2, $state_maintenance2, 0, 0);
 
-           $this->pdf->Ln($line);
-           $this->pdf->SetFont('Arial', '', 8);
-           $this->pdf->Cell(62, 6, '', 0, 0);
-           $this->pdf->Cell(17, 3.2, $state_maintenance1, 0, 0);
-           $this->pdf->Cell(40, 3.2, $t->who, 0, 0);
-           $this->pdf->Cell(45, 6, '', 0, 0);
-           $this->pdf->Cell(23, 3.2, $t->support, 0, 0);
-           $this->pdf->Ln(7);
+            $this->Ln($line);
+            $this->SetFont('Arial', '', 8);
+            $this->Cell(62, 6, '', 0, 0);
+            $this->Cell(17, 3.2, $state_maintenance1, 0, 0);
+            $this->Cell(40, 3.2, $t->who, 0, 0);
+            $this->Cell(45, 6, '', 0, 0);
+            $this->Cell(23, 3.2, $t->support, 0, 0);
+            $this->Ln(7);
 
-           $this->pdf->Cell(1, 6, '', 0, 0);
-            $y =$this->pdf->GetY();
-           $this->pdf->MultiCell(150, 3, utf8_decode($t->observations_responsable), 0, 1);
-            $y2 =$this->pdf->GetY();
+            $this->Cell(1, 6, '', 0, 0);
+            $y = $this->GetY();
+            $this->MultiCell(150, 3, utf8_decode($t->observations_responsable), 0, 1);
+            $y2 = $this->GetY();
 
             $space = (isset($y2) ? $y2 - $y : 0) - 0;
 
@@ -309,9 +304,9 @@ class PrintMaintenanceController extends Fpdf
 
                 $line2 = $line2 + 3;
             }
-            //$this->pdf->SetXY(150, $y);
-            // $this->pdf->Cell(45, 6, $space.' y2 '.$y2.' y1 '. $y, 0, 0);
-           $this->pdf->Ln($space);
+            // $this->SetXY(150, $y);
+            //  $this->Cell(45, 6, $space.' y2 '.$y2.' y1 '. $y, 0, 0);
+            $this->Ln($space);
 
             $line2 = $line2 + 3;
 
@@ -319,6 +314,6 @@ class PrintMaintenanceController extends Fpdf
 
         $header = ['Content-Type' => 'application/pdf'];
 
-       $this->pdf->Output("sdsadasd.pdf", "D");
+        $this->Output("sdsadasd.pdf", "D");
     }
 }
