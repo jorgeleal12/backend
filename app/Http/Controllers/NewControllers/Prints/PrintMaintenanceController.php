b0e0773af89fe7dffb620700d1fe934f0491f965
Andres Leal <sistemas@grupoempresarialcyc.com>
2021-08-18 21:29:38 -0500

    cambio de formato

--- app/Http/Controllers/NewControllers/Prints/PrintMaintenanceController.php
+++ app/Http/Controllers/NewControllers/Prints/PrintMaintenanceController.php
@@ -79,7 +79,7 @@ class PrintMaintenanceController extends Fpdf
         // $this->Cell(23, 3.2,  utf8_decode($service_provided), 0, 0);
         $this->MultiCell(150, 3, utf8_decode($service_provided), 0, 1);
 
-        $this->Ln(6);
+        $this->Ln(2);
         $this->SetFont('Arial', '', 7);
         $this->Cell(3, 6, '', 0, 0);
         $this->Cell(70, 3.2, $search->brand, 0, 0);
