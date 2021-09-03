1613e9321fa48c67e42b640fbdbcc3f638292af1
jorgeleal12 <38261025+jorgeleal12@users.noreply.github.com>
2021-09-03 12:20:12 -0500

    Update PrintMaintenanceController.php

--- app/Http/Controllers/NewControllers/Prints/PrintMaintenanceController.php
+++ app/Http/Controllers/NewControllers/Prints/PrintMaintenanceController.php
@@ -207,6 +207,7 @@ class PrintMaintenanceController extends Fpdf
 
         $search_image = DB::table('image_items')
             ->where('leaves_idleaves', $idleaves)
+            ->where('type_document', '!=', 'pdf')
             ->take(4)
             ->get();
 
@@ -225,12 +226,12 @@ class PrintMaintenanceController extends Fpdf
         foreach ($search_image as $search_image) {
 
             $document= explode('.',$search_image->name_image);
-            if($search_image->type_document!='pdf'){
+           // if($search_image->type_document!='pdf'){
                 $this->Image($search_image->url, $row, 44, 48, 40);
                 $this->Cell(4, 6, '', 0, 0);
                 $row = $row + 48;
                 $row++;
-            }
+           // }
         }
         $name_materials = str_replace('”','"',$search->name_materials);
         $service_provided = str_replace('”','"',$search->service_provided);
